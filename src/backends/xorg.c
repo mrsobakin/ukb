51276b6da578c1fb0d46ae9becbc82796f14bc60
Sobakais <ugoneid@gmail.com>
2025-04-08 14:56:49 +0300

    xorg.c: issues fixed; ukb.c: xorg backend added

--- src/backends/xorg.c
+++ src/backends/xorg.c
@@ -7,56 +7,55 @@
 
 static char current_layout[128] = {0};
 
-static Display *_display = 0;
+static Display *x_display = 0;
 
 static ukb_err_t update_current_layout() {
-	XkbStateRec xkbState;
-	if (XkbGetState(_display, XkbUseCoreKbd, &xkbState) != Success) {
+	XkbStateRec xkb_state;
+	if (XkbGetState(x_display, XkbUseCoreKbd, &xkb_state) != Success) {
     	UKB_ERR("Failed to get keyboard state.");
 	}
 
-	XkbDescPtr descPtr = XkbGetKeyboard(_display, XkbAllComponentsMask, XkbUseCoreKbd);
-	if (!descPtr) {
+	XkbDescPtr desc_ptr = XkbGetKeyboard(x_display, XkbAllComponentsMask, XkbUseCoreKbd);
+	if (!desc_ptr) {
 		UKB_ERR("Failed to get keyboard description.");
 	}
-	if (!descPtr->names) {
+	if (!desc_ptr->names) {
 		UKB_ERR("Failed to get keyboard names.");
 	}
 
-	Status ErrorGetControls = XkbGetControls(_display, XkbAllComponentsMask, descPtr);
-	if (ErrorGetControls != Success || !descPtr->ctrls) {
+	Status error_get_controls = XkbGetControls(x_display, XkbAllComponentsMask, desc_ptr);
+	if (error_get_controls != Success || !desc_ptr->ctrls) {
 		UKB_ERR("Failed to get keyboard controls.");
 	}
 
-	int num_groups = descPtr->ctrls->num_groups;
+	int num_groups = desc_ptr->ctrls->num_groups;
 	if (xkbState.group >= num_groups) {
 		UKB_ERR("Group index out of range.");
 	}
 
-	char *namePtr = XGetAtomName(_display, descPtr->names->groups[xkbState.group]);
-	if (!namePtr) {
+	char *name_ptr = XGetAtomName(x_display, desc_ptr->names->groups[xkb_state.group]);
+	if (!name_ptr) {
 		UKB_ERR("Failed to get group name.");
 	}
 
-	if (strncmp(current_layout, namePtr, sizeof(current_layout)) != 0) {
-		strncpy(current_layout, namePtr, sizeof(current_layout) - 1);
+	if (strncmp(current_layout, name_ptr, sizeof(current_layout)) != 0) {
+		strncpy(current_layout, name_ptr, sizeof(current_layout) - 1);
 	}
 
-	XkbFreeKeyboard(descPtr, 0, True);
-	XFree(namePtr);
+	XkbFreeKeyboard(desc_ptr, 0, True);
+	XFree(name_ptr);
 
 	UKB_OK();
 }
 
 static ukb_err_t xorg_wait_event() {
-
-	Bool bret = XkbSelectEventDetails(_display, XkbUseCoreKbd, XkbStateNotify, XkbAllStateComponentsMask, XkbGroupStateMask);
+	Bool bret = XkbSelectEventDetails(x_display, XkbUseCoreKbd, XkbStateNotify, XkbAllStateComponentsMask, XkbGroupStateMask);
 	if (!bret) {
 		UKB_ERR("XkbSelectEventDetails failed")
 	}
 
 	XEvent event;
-	int iret = XNextEvent(_display, &event);
+	int iret = XNextEvent(x_display, &event);
 	if (!iret) {
 		UKB_ERR("XNextEvent failed");
 	}
@@ -64,29 +63,38 @@ static ukb_err_t xorg_wait_event() {
 	UKB_OK();
 }
 
+static ukb_err_t xorg_listen_loop(ukb_layout_cb_t cb) {
+	while(1) {
+		UKB_PROPAGATE(xorg_wait_event());
+		UKB_PROPAGATE(update_current_layout());
+		cb(current_layout);
+	}
+	UKB_OK();
+}
+
+static int xorg_open_display() {
+	int event_code;
+	int error_return;
+	int major = XkbMajorVersion;
+	int minor = XkbMinorVersion;
+	int reason_return;
+
+	x_display = XkbOpenDisplay(NULL, &event_code, &error_return, &major, &minor, &reason_return);
+	return reason_return;
+}
+
 /********************************* BACKEND ***********************************/
 
 static bool xorg_can_use(void) {
-	int eventCode;
-	int errorReturn;
-	int major = XkbMajorVersion;
-	int minor = XkbMinorVersion;
-	int reasonReturn;
-	_display = XkbOpenDisplay(NULL, &eventCode, &errorReturn, &major, &minor, &reasonReturn);
-	XCloseDisplay(_display);
-	return reasonReturn == XkbOD_Success;
+	int reason_return = xorg_open_display();
+	XCloseDisplay(x_display);
+	return reason_return == XkbOD_Success;
 }
 
 static ukb_err_t xorg_listen(ukb_layout_cb_t cb) {
-	int eventCode;
-	int errorReturn;
-	int major = XkbMajorVersion;
-	int minor = XkbMinorVersion;
-	int reasonReturn;
+	int reason_return = xorg_open_display();
 
-	_display = XkbOpenDisplay(NULL, &eventCode, &errorReturn, &major, &minor, &reasonReturn);
-
-	switch (reasonReturn) {
+	switch (reason_return) {
 	case XkbOD_Success:
 		break;
 	case XkbOD_BadLibraryVersion:
@@ -101,12 +109,9 @@ static ukb_err_t xorg_listen(ukb_layout_cb_t cb) {
 		UKB_ERR("XKB refused to open the display.");
 	}
 
-	while (1) {
-		UKB_PROPAGATE(xorg_wait_event());
-		UKB_PROPAGATE(update_current_layout());
-		cb(current_layout);
-	}
-	XCloseDisplay(_display);
+	ukb_err_t err = xorg_listen_loop(cb);
+	XCloseDisplay(x_display);
+	return err;
 }
 
 const ukb_backend_t ukb_backend_xorg = {
